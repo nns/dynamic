8e1b561ac566f1bdfc4fac6da641b64b6217d738
nns <nozaky2010@gmail.com>
2012-09-07 00:32:39 +0900

    te

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
 });
 
